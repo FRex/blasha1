578002122ccf22d956d51af85d4b5c904d74a8bc
FRex <rex2k8@gmail.com>
2019-12-13 07:17:40 +0100

    made python read in 64 MiB parts

--- sha1.py
+++ sha1.py
@@ -2,7 +2,13 @@ from sys import argv, stderr
 from hashlib import sha1
 
 try:
-    data = open(argv[1], 'rb').read()
-    print(f"{sha1(data).hexdigest()} *{argv[1]}")
+    f = open(argv[1], 'rb')
+    c = sha1()
+    while True:
+        data = f.read(64 * 1024 * 1024)
+        if len(data) == 0:
+            break
+        c.update(data)
+    print(f"{c.hexdigest()} *{argv[1]}")
 except FileNotFoundError as e:
     print(e, file=stderr)
