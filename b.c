8f6bc0f2375abe1189ab55376ce94c99c2ac9130
FRex <rex2k8@gmail.com>
2019-12-26 14:35:27 +0100

    made the program handle multiple files

--- b.c
+++ b.c
@@ -6,16 +6,23 @@
 int main(int argc, char ** argv)
 {
     struct MappedFile file;
-    file.ptr = NULL;
-    memorymapfile(argv[1], &file);
-    if(!file.ptr)
+    int i;
+
+    for(i = 1; i < argc; ++ i)
     {
-        fprintf(stderr, "Failed to open or map file '%s'\n", argv[1]);
-        return 1;
+        file.ptr = NULL;
+        memorymapfile(argv[i], &file);
+        if(!file.ptr)
+        {
+            fprintf(stderr, "Failed to open or map file '%s'\n", argv[i]);
+            continue;
+        }
+
+        char textdigest[41];
+        blasha1_text(file.ptr, file.size, textdigest);
+        printf("%s *%s\n", textdigest, argv[i]);
+        unmapfile(&file);
     }
 
-    char textdigest[41];
-    blasha1_text(file.ptr, file.size, textdigest);
-    printf("%s *%s\n", textdigest, argv[1]);
-    unmapfile(&file);
+    return 0;
 }
